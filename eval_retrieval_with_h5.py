a8eb7c565246ff99c1a6085d9431d590e225054c
GabrielFerrante <gabrielsferrante@hotmail.com>
2026-05-22 03:52:45 -0300

    Results retrieval

--- eval_retrieval_with_h5.py
+++ eval_retrieval_with_h5.py
@@ -289,8 +289,8 @@ if __name__ == "__main__":
 
     aligner = LoRACrossAttentionAligner(visual_dim=768, text_dim=4096, rank=64)
 
-    weights_path = "checkpoints/best_aligner_no_up.pth"
-    "weights_path = checkpoints/best_aligner.pth"
+    #weights_path = "checkpoints/best_aligner_no_up.pth"
+    weights_path = "checkpoints/best_aligner.pth"
     if os.path.exists(weights_path):
         aligner.load_state_dict(
             torch.load(weights_path, map_location=device),
@@ -302,7 +302,8 @@ if __name__ == "__main__":
 
     aligner.to(device).to(torch.bfloat16).eval()
 
-    test_h5_ds = ShardedH5Dataset_withSSD("E:/COYO/embeds/test_noup") #ESSE SEM UP
+    test_h5_ds = ShardedH5Dataset_withSSD("E:/COYO/embeds/test_noup") #WITHOUT ANYUP
+    #test_h5_ds = ShardedH5Dataset_withSSD("G:/coyo/embeds/test_anyup") #WITH ANYUP
     test_h5_loader = DataLoader(
         test_h5_ds, batch_size=128, shuffle=False,
         pin_memory=True, num_workers=4, prefetch_factor=4, persistent_workers=True,
